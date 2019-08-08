18c3f0596383c899773d2fbeea539a58ebc1368e
chaorenzeng <921435247@qq.com>
2019-08-08 10:40:41 +0800

    v2.0.0

--- README.md
+++ README.md
@@ -1,29 +1,34 @@
 ## NumScroll
 #### 数字滚动累加动画插件 
-1.使用前先引入jquery  
-2.前端学习群:814798690
+1.使用前先引入jquery  (Introduce jQuery before use)
+2.加入学习Q群:814798690 (Join Learning QQ Group: 814798690)
 #### 快速使用
 1.引入jquery和jquery.numscroll.js
 ```js
-<script src='http://code.jquery.com/jquery-2.1.1.min.js' type='text/javascript'></script>
+<script src="js/jquery-1.10.2.min.js" type="text/javascript" charset="utf-8"></script>
 <script src="js/jquery.numscroll.js" type="text/javascript" charset="utf-8"></script>
 ```
-2.拷贝以下布局结构
+2.拷贝以下布局结构 (Copy the following layout structure)
 ```html
-<!--默认写法-->
-<span class="num">888888</span>
-<!--推荐写法-->
-<span class="num" data-num="888888"></span>
+<!--默认写法 Default Writing-->
+<p><span id="num0">888,692,346,813</span></p>
+<!--推荐写法 Recommended Writing-->
+<p><span id="num1" data-num="888692346813.89"></span></p>
+<p><span id="num2" data-num="888692346813.89"></span></p>
 ```
-3.创建numscroll对象:
+3.创建numscroll对象 (Create numscroll objects)
 ```js
-$(".num").numScroll();
+$('#num0').numScroll()
+$('#num1').numScroll()
+$('#num2').numScroll({
+	'symbol': true
+})
 ```
 #### API文档
-可选参数  |  默认值 | 说明
+可选参数 Parameter |  默认值 Default | 说明 Introduce
 --        |    --   | --
-time      |   1500  | 滚动总时长
-delay     |   0     | 延迟启动时长
-
+time      |   1500  | 持续时间 durationd
+delay     |   0     | 延迟开始 delay
+symbol    |   0     | 是否显示分隔符 display separators
 #### 案例展示
 ![查看演示](https://github.com/chaorenzeng/jquery.numscroll.js/blob/master/index.gif)
--- js/jquery.numscroll.js
+++ js/jquery.numscroll.js
@@ -1,84 +1,150 @@
 /*!
  * jquery.numscroll.js -- 数字滚动累加动画插件  (Digital rolling cumulative animation)
- * version 1.0.0
- * 2018-09-22
+ * version 2.0.0
+ * 2019-08-08
  * author: KevinTseng < 921435247@qq.com@qq.com >
- * 文档:  https://github.com/chaorenzeng/jquery.numscroll.js.git
- * QQ交流群: 739574382
+ * API文档: https://github.com/chaorenzeng/jquery.numscroll.js.git
+ * 交流Q群: 814798690
  */
 
 (function($) {
 	
-	function isInt(num) {
-		//作用:是否为整数
-		//返回:true是 false否
-		var res = false;
-		try {
-			if(String(num).indexOf(".") == -1 && String(num).indexOf(",") == -1) {
-				res = parseInt(num) % 1 === 0 ? true : false;
-			}
-		} catch(e) {
-			res = false;
-		}
-		return res;
-	}
-
-	function isFloat(num) {
-		//作用:是否为小数
-		//返回:小数位数(-1不是小数)
-		var res = -1;
-		try {
-			if(String(num).indexOf(".") != -1) {
-				var index = String(num).indexOf(".") + 1; //获取小数点的位置
-				var count = String(num).length - index; //获取小数点后的个数
-				if(index > 0) {
-					res = count;
-				}
-			}
-		} catch(e) {
-			res = -1;
-		}
-		return res;
-	}
-
 	$.fn.numScroll = function(options) {
 		
 		var settings = $.extend({
-			'time': 1500,
-			'delay': 0
+			'time': 1500, //持续时间 durationd
+			'delay': 0,	//延迟开始 delay
+			'symbol': false //是否显示分隔符 display separators
 		}, options);
 		
 		return this.each(function() {
+			//初始化
 			var $this = $(this);
 			var $settings = settings;
-			
-			var num = $this.attr("data-num") || $this.text(); //实际值
-			var temp = 0; //初始值
+			var source = $this.attr("data-num") || $this.text();
+			var temp = 0;
 			$this.text(temp);
-			var numIsInt = isInt(num);
-			var numIsFloat = isFloat(num);
-			var step = (num / $settings.time) * 10; //步长
+			//分隔符显示判断
+			if (source.indexOf(',') > 0) {
+				//数值含有分隔符，则默认为需要显示分隔符
+				$settings.symbol = true;
+			}
+			if(options && !options.symbol){
+				//手动设置不显示分隔符时，不显示分隔符
+				$settings.symbol = false;
+			}
+			//分隔符过滤
+			var num = source.replace(/,/g, ''), numScroll;
+			var numIsInt = isInt(num),
+				numIsFloat = isFloat(num),
+				step = (num / $settings.time) * 10; //步长
+				
+			//增长方法
+			function numInitGrow() {
+				var showNum = '';
+				//整型或浮点型
+				if (numIsInt) {
+					showNum = Math.floor(temp);
+				} else if (numIsFloat != -1) {
+					showNum = temp.toFixed(numIsFloat)
+				} else {
+					showTarget(source);
+					clearInterval(numScroll);
+					return;
+				}
+				//千位符显示
+				if ($settings.symbol) {
+					showNum = formatSymbol(showNum);
+				}
+				$this.text(showNum);
+			}
 			
+			//最终显示
+			function showTarget(num){
+				var targetNum = num.replace(/,/g, '');
+				if ($settings.symbol) {
+					targetNum = formatSymbol(targetNum);
+				}
+				$this.text(targetNum);
+			}
+			
+			//定时开始
 			setTimeout(function() {
-				var numScroll = setInterval(function() {
-					if(numIsInt) {
-						$this.text(Math.floor(temp));
-					} else if(numIsFloat != -1) {
-						$this.text(temp.toFixed(numIsFloat));
-					} else {
-						$this.text(num);
-						clearInterval(numScroll);
-						return;
-					}
+				numScroll = setInterval(function() {
+					numInitGrow();
 					temp += step;
-					if(temp > num) {
-						$this.text(num);
+					if (temp > num) {
+						showTarget(source);
 						clearInterval(numScroll);
 					}
 				}, 1);
 			}, $settings.delay);
-			
 		});
 	};
-
+	
+	/*	
+	* 判断数值是否为整数
+	* @param num {Number} 数值
+	* @return {Boolean} 真假
+	*/
+	function isInt(num) {
+		var res = false;
+		try {
+			if (String(num).indexOf(".") == -1 && String(num).indexOf(",") == -1) {
+				res = parseInt(num) % 1 === 0 ? true : false;
+			}
+		} catch (e) {
+			res = false;
+		}
+		return res;
+	}
+	
+	/*	
+	* 判断数值是否为小数
+	* @param num {Number} 数值
+	* @return {Number} 小数位数(-1时不是小数)
+	*/
+	function isFloat(num) {
+		var res = -1;
+		try {
+			if (String(num).indexOf(".") != -1) {
+				var index = String(num).indexOf(".") + 1; //获取小数点的位置
+				var count = String(num).length - index; //获取小数点后的个数
+				if (index > 0) {
+					res = count;
+				}
+			}
+		} catch (e) {
+			res = -1;
+		}
+		return res;
+	}
+	
+	/*	
+	* 显示数值千分位分隔符
+	* @param num {Number} 数值
+	* @return {String} 含分隔符数值
+	*/
+	function formatSymbol(num) {
+		var res = '';
+		var str = num + '',strLeft='',strRight='';
+		var floatNum = isFloat(num);
+		if(floatNum!=-1){
+			//有小数时进行切割
+			var splitStr = str.split('.');
+			strLeft = splitStr[0];
+			strRight = splitStr[1];
+		}else{
+			strLeft = str;
+		}
+		//整数部分每隔3位添加分隔符
+		res = strLeft.split("").reverse().reduce(function(prev, next, index) {
+			return ((index % 3) ? next : (next + ',')) + prev;
+		})
+		//拼接小数部分
+		if(strRight!=''){
+			res = res+'.'+strRight;
+		}
+		return res;
+	}
 })(jQuery);
